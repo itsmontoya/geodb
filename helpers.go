def9edac3b88b7354bde2958090a987a9c39d837
Josh Montoya <josh@montoya.io>
2023-02-24 17:33:47 -0800

    remove non-needed funcs

--- helpers.go
+++ helpers.go
@@ -13,19 +13,7 @@ const (
 	radianMultiplier = (math.Pi / 180.0)
 	// Meters to foot conversion value
 	metersInFoot = 0.3048
-	// Feet to miles conversion value
-	feetInMile = 5280
 )
-
-// ToMeters will convert a foot value to meters
-func ToMeters(feet float64) (meters float64) {
-	return feet * metersInFoot
-}
-
-// MilesToMeters will return the number of meters in a provided number of miles
-func MilesToMeters(miles float64) (meters float64) {
-	return ToMeters(miles * feetInMile)
-}
 
 // GetDistance will return distance
 func GetDistance(a, b Location) Unit {
