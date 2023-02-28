c2fcefdb3ef00ca5b248758c6f26a7cfd1a1fcd0
Josh Montoya <josh@montoya.io>
2023-02-28 12:36:14 -0800

    implement test for NewPolygon and Polygon.Radius

--- polygon_test.go
+++ polygon_test.go
@@ -5,6 +5,77 @@ import (
 	"testing"
 )
 
+func TestNewPolygon(t *testing.T) {
+	type args struct {
+		locs []Location
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "four locations",
+			args: args{
+				locs: []Location{
+					MakeLocation(0, 0),
+					MakeLocation(0, 4),
+					MakeLocation(4, 4),
+					MakeLocation(4, 0),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "three locations",
+			args: args{
+				locs: []Location{
+					MakeLocation(0, 0),
+					MakeLocation(0, 4),
+					MakeLocation(4, 4),
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "two locations",
+			args: args{
+				locs: []Location{
+					MakeLocation(0, 0),
+					MakeLocation(0, 4),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "one location",
+			args: args{
+				locs: []Location{
+					MakeLocation(0, 0),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty",
+			args: args{
+				locs: []Location{},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewPolygon(tt.args.locs)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewPolygon() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
 func TestPolygon_IsWithin(t *testing.T) {
 	type fields struct {
 		locs []Location
@@ -99,6 +170,44 @@ func TestPolygon_Center(t *testing.T) {
 
 			if got := p.Center(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Polygon.Center() = %v, want %v", got.String(), tt.want.String())
+			}
+		})
+	}
+}
+
+func TestPolygon_Radius(t *testing.T) {
+	type fields struct {
+		locations []Location
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		want   Meter
+	}{
+		{
+			name: "basic",
+			fields: fields{
+				locations: []Location{
+					MakeLocation(0, 0),
+					MakeLocation(0, 4),
+					MakeLocation(4, 4),
+					MakeLocation(4, 0),
+				},
+			},
+			want: NewLocation(2, 2).Distance(MakeLocation(0, 0)),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p, err := NewPolygon(tt.fields.locations)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got := p.Radius(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Polygon.Center() = %v, want %v", got, tt.want)
 			}
 		})
 	}
