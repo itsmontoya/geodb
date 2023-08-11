0d72bb7b3cd0207c3b97fca0049ef9594831fbe7
Josh Montoya <josh@montoya.io>
2023-08-11 13:06:42 -0700

    implement Float64 and String methods

--- degree.go
+++ degree.go
@@ -1,8 +1,18 @@
 package geodb
 
+import "strconv"
+
 type Degree float64
 
 // toRadians will convert degrees to radians
 func (d Degree) toRadians() Radian {
 	return Radian(float64(d) * radianMultiplier)
+}
+
+func (d Degree) Float64() float64 {
+	return float64(d)
+}
+
+func (d Degree) String() string {
+	return strconv.FormatFloat(d.Float64(), 'f', -1, 64)
 }
