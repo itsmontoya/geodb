e77aa0821233a628f627b92cd930cf2afbbf8198
Josh Montoya <josh@montoya.io>
2024-09-15 21:20:56 -0700

    add cosine func

--- radian.go
+++ radian.go
@@ -1,8 +1,14 @@
 package geodb
 
+import "math"
+
 type Radian float64
 
 // toDegrees will convert radians to degrees
 func (r Radian) toDegrees() Degree {
 	return Degree(float64(r) / radianMultiplier)
+}
+
+func (r Radian) cosine() float64 {
+	return math.Cos(float64(r))
 }
